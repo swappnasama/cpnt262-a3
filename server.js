d1339a5338168d8d907ad49fe34f9ed69e36f696
Swapna <sama.swapna@edu.sait.ca>
2021-03-12 15:05:38 -0700

    added comments to server.js and page not founf page

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
+// Importing require modules
 const express = require('express');
 const app=express();
-
+//response to server
 app.use(express.static('./public'));
 
-const url= require('url');
+//404 file not found redirection
 app.use(function(req,res){
   res.status(404);
   res.redirect('/404.html');
   
 });
 
+// hosting the server
 const PORT=process.env.PORT || 3000;
 app.listen(PORT,function(){
   console.log(`Listening on port:${PORT}`);
